58dec766bb0cd1ebdd2f3c48e8528982675d50c2
Dane Schmidt <daneschmidt@gmail.com>
2019-11-23 09:56:32 -0600

    server is setup and running and the get is working so far.  need to setup the POST

--- server/server.js
+++ server/server.js
@@ -4,8 +4,22 @@ const PORT = process.env.PORT || 5000;
 const bodyParser = require('body-parser');
 
 app.use(express.static('server/public'));
+// These make req.body available!
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json());
 
+const stuffToCalc = {
+    value1: 2,
+    value2: 2,
+    operator: 'multiply'  //change fix or address this later... im not sure how this is going to work yet... idea
+                          //now is to take the 'multiply' and make an if then check to swap 'multiply' for * etc
+}
+
+app.get('/api/result', (req,res) => {
+    res.send(stuffToCalc);
+});
 
 
+app.listen(PORT, () => {
+    console.log(`What up from port: ${PORT}`);
+})
