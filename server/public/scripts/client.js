6effbeec1186ac25b44e5fd60ecac648f2a7a617
Dane Schmidt <daneschmidt@gmail.com>
2019-11-24 16:39:51 -0600

    making sure im current

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -6,13 +6,21 @@ function init () {
 
     $('.js-submit-btn').on('click', onClickEqual);
 
+    $('.js-minus-btn').on('click', testfunction);
+
 }
 
+
+function testfunction() {
+    console.log('- button was clicked');
+
+}
 function onClickEqual(event) {
-    console.log('you just clicked the button');
+    console.log('you just clicked the = button');
     const postPack = {
     input1 = $('.js-input1').val(),
     input2 = $('.js-input2').val(),
+    opInput = "add",
     }
-    console.log(input1, input2);
+    console.log(postPack);
 }
