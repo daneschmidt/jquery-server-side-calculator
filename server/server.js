618559c06ccf441b54095ad0683bc1abcfe77b3c
Dane Schmidt <daneschmidt@gmail.com>
2019-11-24 13:17:19 -0600

    addCalc is starting to work but the math isnt right yet

--- server/server.js
+++ server/server.js
@@ -3,28 +3,42 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 const bodyParser = require('body-parser');
 
-
-let calcObject = [];
-
-
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json())
 app.use(express.static('server/public'));
 
+//going to move this next section to a module
+
+let calcArray = [];
+
 
 function addCalc(newCalc) {
-    calcObject.push(newCalc)
-    console.log(calcObject);
+    calcArray.push(newCalc)
+    console.log(calcArray);
 
 }
 
+//end move to module section
 
 app.post('/api/toCalc', (req,res) => {
     // console.log(req.body);
     const newCalc = req.body;
+    const opInput = req.body.opInput;
+    const input1 = req.body.input1;
+    const input2 = req.body.input2;
+    
     addCalc(newCalc);
     res.sendStatus(200);
     // console.log(newCalc);
+    let answer;
+
+    if(opInput == "add") {
+        answer = input1 + input2;
+        console.log(answer);
+
+    }else {
+        console.log('try again');
+    }
 });
 
 
