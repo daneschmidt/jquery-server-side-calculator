d124fe06b83945eaa9974b0ea2efa51b70feb5f8
Dane Schmidt <daneschmidt@gmail.com>
2019-11-24 17:44:01 -0600

    made functions triggered based on whichever math op button is clicked to set the inputOp value to a string with the label of the math op to be applied

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -4,23 +4,47 @@ $(document).ready(init);
 function init () {
     console.log ('Welcome. Blue Warrior is now IT');
 
-    $('.js-submit-btn').on('click', onClickEqual);
+    $('#js-submit-btn').on('click', onClickEqual);
 
-    $('.js-minus-btn').on('click', testfunction);
+    $('#js-minus-btn').on('click', clickSub);
+    $('#js-plus-btn').on('click', clickAdd);
+    $('#js-times-btn').on('click', clickMulti);
+    $('#js-divide-btn').on('click', clickDivide);
+
+}
+
+
+function clickAdd(event) {
+    opInput = "add";
+    console.log('add button clicked');
+
+}
+
+function clickSub(event) {
+    opInput = "sub";
+    console.log('sub button clicked');
+
+}
+
+function clickMulti(event) {
+    opInput = "multi";
+    console.log('multi button clicked');
+
+}
+
+function clickDivide(event) {
+    opInput = "divide";
+    console.log('divide button clicked');
 
 }
 
 
-function testfunction() {
-    console.log('- button was clicked');
-
-}
 function onClickEqual(event) {
     console.log('you just clicked the = button');
     const postPack = {
-    input1 = $('.js-input1').val(),
-    input2 = $('.js-input2').val(),
-    opInput = "add",
+    input1: $('#js-input1').val(),
+    input2: $('#js-input2').val(),
+    opInput: "add",
     }
     console.log(postPack);
 }
